0c1cca7fb67dc4a0471243f2c69cfcd8c34ae958
Stephen Fry <sfry@financialforce.com>
2018-05-09 17:36:52 +0100

    Run ncu to update dependencies and fix lint issues.

--- src/lib/index/publisher.js
+++ src/lib/index/publisher.js
@@ -2,22 +2,22 @@
  * Copyright (c) 2017-2018, FinancialForce.com, inc
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification, 
+ * Redistribution and use in source and binary forms, with or without modification,
  *   are permitted provided that the following conditions are met:
  *
- * - Redistributions of source code must retain the above copyright notice, 
+ * - Redistributions of source code must retain the above copyright notice,
  *      this list of conditions and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright notice, 
- *      this list of conditions and the following disclaimer in the documentation 
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
  *      and/or other materials provided with the distribution.
- * - Neither the name of the FinancialForce.com, inc nor the names of its contributors 
- *      may be used to endorse or promote products derived from this software without 
+ * - Neither the name of the FinancialForce.com, inc nor the names of its contributors
+ *      may be used to endorse or promote products derived from this software without
  *      specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
- *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
- *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
- *  THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ *  THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
  *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  *  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
  *  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
@@ -42,15 +42,15 @@ const
 	ERROR_EVENT = 'error_event',
 	INFO_EVENT = 'info_event';
 
-/** 
+/**
  * The Publisher for publishing messages based on Avro schemas.
  * @extends EventEmitter
- **/
+ */
 class Publisher extends EventEmitter {
 
 	/**
 	 * Constructs a new 'Publisher'.
-	 * 
+	 *
 	 * @param {Object} config - { transport [, transportConfig] }
 	 * @param {transport} config.transport - the transport object
 	 * @param {Object} config.transportConfig - config for the transport object
@@ -84,19 +84,19 @@ class Publisher extends EventEmitter {
 
 	/**
 	 * Publishes a message.
-	 * 
+	 *
 	 * @example
 	 * // publishes a message
 	 * publisher.publish({ schema, message });
 	 * @example
 	 * // publishes a message
 	 * publisher.publish({ schema, message, context });
-	 * 
+	 *
 	 * @param {Object} config - The message arguments.
 	 * @param {Object} config.schema - The Apache Avro schema.
 	 * @param {Object} config.message - The message to send.
 	 * @param {Object} config.context - The context for this message.
-	 * 
+	 *
 	 * @returns {Promise} A promise.
 	 */
 	publish(config) {
