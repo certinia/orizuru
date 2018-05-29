e8c15502d43835a0fc62c8a6750730ac18cfaf6d
Matthew Wood <mwood@financialforce.com>
2018-05-29 08:42:01 +0100

    Update import statement

--- test/index/server/routeMethod.test.ts
+++ test/index/server/routeMethod.test.ts
@@ -24,7 +24,7 @@
  *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import chai, { expect } from 'chai';
+import { expect } from 'chai';
 
 import * as routeMethod from '../../../src/index/server/routeMethod';
 
