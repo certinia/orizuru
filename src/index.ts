0e99db11286227d36c4386616cbfa2b4f514f171
Matthew Wood <mwood@financialforce.com>
2019-01-31 10:32:00 +0000

    Export the RequestHandler from express

--- src/index.ts
+++ src/index.ts
@@ -52,7 +52,7 @@ export { Publisher } from './index/publisher';
  */
 export { Server } from './index/server';
 
-export { json, urlencoded, Request, Response, NextFunction, static as addStaticRoute } from 'express';
+export { json, urlencoded, Request, RequestHandler, Response, NextFunction, static as addStaticRoute } from 'express';
 
 declare global {
 
